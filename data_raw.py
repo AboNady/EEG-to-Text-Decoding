b3b59f1e6a0a9cbcb67e14b9e6d22f3006f1bc1f
Mahmoud Nady <111538752+AboNady@users.noreply.github.com>
2024-10-28 20:50:08 +0300

    Update data_raw.py

--- data_raw.py
+++ data_raw.py
@@ -10,7 +10,7 @@ from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_se
 
 
 ZUCO_SENTIMENT_LABELS = json.load(open('/content/EEG-to-Text-Decoding/dataset/ZuCo/task1-SR/sentiment_labels/sentiment_labels.json'))
-SST_SENTIMENT_LABELS = json.load(open('content/EEG-to-Text-Decoding/dataset/stanfordsentiment/stanfordSentimentTreebank/ternary_dataset.json'))
+SST_SENTIMENT_LABELS = json.load(open('/content/EEG-to-Text-Decoding/dataset/stanfordsentiment/stanfordSentimentTreebank/ternary_dataset.json'))
 
 from scipy.signal import butter, lfilter
 from scipy.signal import freqz
